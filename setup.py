b32906fac13a46707a691372342fab36b1f0cd19
andrewm4894 <andrewm4894@gmail.com>
2019-11-04 21:12:40 +0000

    Bump version: 0.2.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/andrewm4894/am4894dev',
-    version='0.2.0',
+    version='version='0.2.0'',
     zip_safe=False,
 )
