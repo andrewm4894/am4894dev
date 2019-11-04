df6a266418b17b11c626d818fce8eb9a0933e065
andrewm4894 <andrewm4894@gmail.com>
2019-11-04 21:17:07 +0000

    bumpversion minor

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/andrewm4894/am4894dev',
-    version='version='0.3.0'',
+    version='0.3.0',
     zip_safe=False,
 )
