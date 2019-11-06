293d1713e46bb77fb0b7de723762f4ef2f25f320
andrewm4894 <andrewm4894@gmail.com>
2019-11-06 21:31:10 +0000

    drop py35

--- setup.py
+++ setup.py
@@ -20,14 +20,12 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Andrew Maguire",
     author_email='andrewm4894@gmail.com',
-    python_requires='>=3.5, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
